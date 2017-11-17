42204c1d5717d2e2fce5caa0e2d82de5ba0d8b2e
hasitha madusanka <hasithamadusanka90_@outlook.com>
2017-11-17 16:27:39 +0530

    add custom DB downgrate and update in the migration

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AspnetCore_angular04_EntityFramework_Vega_2.Models
 {
+    [Table("Makes")]
     public class Make
     {
         public Make()
@@ -13,6 +15,7 @@ namespace AspnetCore_angular04_EntityFramework_Vega_2.Models
         public int Id { get; set; } 
 
         [Required]
+        [StringLength(255)]
         public string Name { get; set; }    
 
         public ICollection<Model> Models { get; set; }    
